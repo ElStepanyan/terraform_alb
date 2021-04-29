e29fef3e65f1d7ad4843e9393f9097b01e986114
ElStepanyan <ElStepanyan>
2021-04-29 09:11:37 +0000

    ansible exec fixed

--- ansible.tf
+++ ansible.tf
@@ -1,3 +1,9 @@
-provisioner "local-exec" {
+resource "null_resource" "install_env" {
+
+  provisioner "local-exec" {
     command = "ansible-playbook -u ubuntu ../wordpress.yml"
+  }
+
+  depends_on = [aws_instance.wp_1, aws_instance.wp_2]
+
 }
