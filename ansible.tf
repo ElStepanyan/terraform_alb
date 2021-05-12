3e3cc9e11770dacb0585ef6b31048d874cbfa4d4
ElStepanyan <ElStepanyan>
2021-05-12 10:35:56 +0000

    fixed ansible ssh connect

--- ansible.tf
+++ ansible.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "install_env" {
 
   provisioner "local-exec" {
-    command = "( cd epam_trainig_ansible_roles && ANSIBLE_HOST_KEY_CHECKING=false | ansible-playbook -u ubuntu wordpress.yml)"
+    command = "( cd epam_trainig_ansible_roles && ansible-playbook -u ubuntu wordpress.yml)"
   }
 
   depends_on = [aws_instance.wp_1, aws_instance.wp_2]
